8126a8d71846495c992d2281ac5da02e147700d7
刘俊杰 <lqlw@me.com>
2021-09-29 16:53:10 +0800

    a

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,114 +1,53 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+    <div style="margin-bottom: 5px;">
+      <input v-model="num">
+      <input v-model="startTime">
+      <input v-model="endTime">
+    </div>
+    <div class="bgc" v-for="item in parseInt(num)" :key="item">
+      <span>NO:LSYY{{item.toString().length === 3? `${item}` : item.toString().length === 2? `0${item}` : `00${item}`}}</span>
+      <div style="height: 110px;width: fit-content;">
+      </div>
+      <div style="font-size:8px;font-weight: bold;margin-left: 12px;">
+        车牌号码：________；<br>
+        泊车日期/时间：_____年____月____日____时____分；<br>
+        取车时间：____时 ____分 ；收费员签署：<br>
+        &nbsp;&nbsp;&nbsp; 本券有效期：{{startTime}}至{{ endTime }}<br>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "HelloWorld",
-  props: {
-    msg: String,
+  data(){
+    return{
+      num:1,
+      startTime:'2021年10月 1 日',
+      endTime:'2021年10月31日'
+    }
   },
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.hello{
+  width: 100vw;
+  height: auto;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
+.bgc{
+  height: 181px;
+  width: 300px;
+  border: 2px solid #000;
+  background: url("../assets/logo.png") no-repeat;
   display: inline-block;
-  margin: 0 10px;
+  margin: 5px;
 }
-a {
-  color: #42b983;
+span{
+  float: right;
+  margin-right: 10px;
+  font-size: 8px;
 }
 </style>
